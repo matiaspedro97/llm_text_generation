7cd3777a8ad84ccbb5a8da9feed45841891f22e7
matiaspedro97 <matiaspedro97@gmail.com>
2024-05-01 16:20:55 +0100

    feat: added project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring the text generation capabilities of the most recent Large Language Models',
+    author='Pedro Matias',
+    license='MIT',
+)
